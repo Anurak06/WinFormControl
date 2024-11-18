f232df1b9ffa883b44d3990bd06482356b38e800
Anurak06 <0942713622ball@gmail.com>
2024-11-18 11:59:43 +0700

    Add project files.

---
+++ WinFormControl/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WinFormControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(865, 46);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 55);
+            button1.TabIndex = 0;
+            button1.Text = "ปิดโปรแกรม";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(865, 118);
+            button2.Name = "button2";
+            button2.Size = new Size(114, 60);
+            button2.TabIndex = 1;
+            button2.Text = "เปิด";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            button2.MouseLeave += button2_MouseLeave;
+            button2.MouseHover += button2_MouseHover;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(865, 192);
+            button3.Name = "button3";
+            button3.Size = new Size(114, 60);
+            button3.TabIndex = 2;
+            button3.Text = "ปิด";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            button3.MouseLeave += button3_MouseLeave;
+            button3.MouseHover += button3_MouseHover;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.White;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(175, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(541, 55);
+            label1.TabIndex = 3;
+            label1.Text = "มหาวิทยาลัยราชภัฎอุดรธานี";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
+            label1.MouseLeave += label1_MouseLeave;
+            label1.MouseHover += label1_MouseHover;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(175, 209);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(541, 127);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "000000000000000000";
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.DoubleClick += textBox1_DoubleClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(1029, 630);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "WinFormControl";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            Click += Form1_Click;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private TextBox textBox1;
+    }
+}
